f45e5de5e950ce0db2fd0cba5cc5d057f28842dd
aKowa <kowaandre@gmail.com>
2016-10-21 19:20:39 +0200

    [ADDED] basic functionality

---
+++ Assets/Scripts/CardComponent.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class CardComponent : MonoBehaviour
+{
+	public Policy[] policies = new Policy[2];
+
+	public RectTransform policyCardRoot;
+	public GameObject policyCard;
+
+	public void Start ()
+	{
+		SetRandomPolicy();
+	}
+
+	public void SetRandomPolicy ()
+	{
+		ClearChildren ();
+
+		for(int i = 0; i < policies.Length; i++)
+		{
+			policies[i] = GetRandomPolicy ();
+			var clone = Instantiate ( policyCard );
+			clone.transform.SetParent( policyCardRoot );
+			var trans = clone.GetComponent<RectTransform> ();
+			trans.localScale = Vector3.one;
+
+			var texts = clone.GetComponentsInChildren<Text> ();
+			texts[0].text = policies[i].name.ToString();
+			texts[1].text = policies[i].value.ToString();
+
+			if ( policies[i].value < 0)
+			{
+				texts[0].color = Color.red;
+				texts[1].color = Color.red;
+			}
+		}
+	}
+
+	private Policy GetRandomPolicy ()
+	{
+		var e = Enum.GetValues ( typeof ( PolicyName ) );
+		return new Policy
+		{
+			name = (PolicyName)e.GetValue ( UnityEngine.Random.Range ( 0, e.Length ) ),
+			value = UnityEngine.Random.Range ( -20, 20 )
+		};
+	}
+
+	private void ClearChildren ()
+	{
+		if (policyCardRoot.childCount > 0)
+		{
+			var children = policyCardRoot.transform.GetComponentsInChildren<Transform> ();
+			for (int i = 1; i < children.Length; i++)
+			{
+				Destroy ( children[i].gameObject );
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public CardComponent card;
+
+	public void Update ()
+	{
+		for (int i = 0; i <= 1; i++)
+		{
+			if (Input.GetMouseButtonDown ( i ))
+			{
+				var cardPolicy = card.policies[i];
+				foreach (var policyComponent in this.GetComponentsInChildren<PolicyUIComponent> ())
+				{
+					if ( policyComponent.policy.name == cardPolicy.name)
+					{
+						policyComponent.AddPolicy ( cardPolicy );
+						break;
+					}
+				}
+
+				card.SetRandomPolicy ();
+			}
+		}
+	}
+}

9be00a48d4edd226194b58de6e82f2c19223535e
Andre Kowalewski <kowaandre@gmail.com>
2017-01-14 21:18:18 +0100

    FIXED: GameManager merge conflict

--- Assets/Content/Scripts/GameManager.cs
+++ Assets/Content/Scripts/GameManager.cs
@@ -21,45 +21,11 @@ namespace Content.Scripts
         private List<CardData> cardData; 
 
         /// <summary>
-        /// The card.
-        /// </summary>
-        public CardComponent card;
-
-        /// <summary>
         /// The start.
         /// </summary>
         public void Start()
         {
             this.cardData = CardDataLoader.GetCardDataList();
-        }
-
-        /// <summary>
-        /// The update.
-        /// </summary>
-        public void Update()
-        {
-            for (int i = 0; i <= 1; i++)
-            {
-                if (Input.GetMouseButtonDown(i))
-                {
-                    if (this.card == null) // sanity check
-                    {
-                        return;
-                    }
-
-                    var cardPolicy = this.card.policies[i];
-                    foreach (var policyComponent in this.GetComponentsInChildren<PolicyUIComponent>())
-                    {
-                        // if ( policyComponent.policy.name == cardPolicy.name)
-                        // {
-                        // 	policyComponent.AddPolicy ( cardPolicy );
-                        // 	break;
-                        // }
-                    }
-
-                    this.card.SetRandomPolicy();
-                }
-            }
         }
     }
 }

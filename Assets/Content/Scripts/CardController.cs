4073486539e24c838833a556459497d0298122ae
Andre Kowalewski <kowaandre@gmail.com>
2017-01-14 17:05:24 +0100

    ADDED: Card rotation on input

---
+++ Assets/Content/Scripts/CardController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardController : MonoBehaviour
+{
+	public float maxAngle = 15f;
+
+	/// <summary>
+	/// Rotates the card towards the input position when dragged.
+	/// </summary>
+	public void OnDrag()
+	{
+		var targetDirection = ( Input.mousePosition - this.transform.position ).normalized;
+		var angle = Vector3.Angle( targetDirection, Vector3.up );
+		angle = Mathf.Clamp( angle, 0, maxAngle );
+		if ( Input.mousePosition.x > this.transform.position.x )
+		{
+			angle *= -1;
+		}
+		this.transform.rotation = Quaternion.Euler( 0.0f, 0.0f, angle );
+	}
+
+
+	public void OnEndDrag()
+	{
+		
+	}
+}

1a2f77ad17b12549e38082e1788574ba14a6e922
Andre Kowalewski <kowaandre@gmail.com>
2017-03-27 14:45:58 +0200

    [ADDED] credit scene and scene Loader

---
+++ Assets/Content/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	/// <summary>
+	/// The scene ID to be Loaded.
+	/// </summary>
+	[Tooltip( "The scene ID to be Loaded. Make this " )]
+	public int id = 1;
+
+	/// <summary>
+	/// Loads the scene with the specified ID.
+	/// </summary>
+	public void LoadScene ()
+	{
+		SceneManager.LoadScene( id );
+	} 
+}

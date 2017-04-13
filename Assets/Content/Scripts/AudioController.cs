afad21199e35fe581024973a38459f77692d3df2
Andre Kowalewski <kowaandre@gmail.com>
2017-04-13 15:08:26 +0200

    [ADDED] sounds and sound controller

---
+++ Assets/Content/Scripts/AudioController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+namespace Content.Scripts
+{
+	public class AudioController : MonoBehaviour
+	{
+		/// <summary>
+		/// Background music source
+		/// </summary>
+		[SerializeField]
+		private AudioSource bgMusic = null;
+
+		/// <summary>
+		/// Sound source
+		/// </summary>
+		[SerializeField]
+		private AudioSource sound = null;
+
+		/// <summary>
+		/// internal instance
+		/// </summary>
+		private static AudioController instance;
+
+		/// <summary>
+		/// external instance
+		/// </summary>
+		public static AudioController Instance
+		{
+			get { return instance; }
+		}
+
+		/// <summary>
+		/// Sets static instance 
+		/// </summary>
+		private void Awake ()
+		{
+			instance = this;
+		}
+
+
+		private void Mute ( bool state )
+		{
+			this.gameObject.SetActive ( state );
+		}
+	}
+}
---
+++ Assets/Content/Scripts/MuteController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Content.Scripts
+{
+	public class MuteController : MonoBehaviour
+	{
+		private Toggle toggle = null;
+
+		private void Start ()
+		{
+
+		}
+	}
+}
